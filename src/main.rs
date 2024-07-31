b51cb60be098bb352b0fed08b918694ada0f4ac3
MaximilianHausen <Max.Ha@directbox.com>
2024-07-31 02:33:52 +0200

    Auto-leave on inactivity + basic queue + Youtube search autocomplete

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-mod music_commands;
-
-use log::{error, info};
-use reqwest::Client as HttpClient;
-use serenity::all::GuildId;
-use serenity::prelude::*;
-use serenity::Client;
-use songbird::tracks::TrackHandle;
-use songbird::SerenityInit;
 use std::collections::HashMap;
 use std::env;
 
+use log::{error, info, LevelFilter};
+use poise::FrameworkContext;
+use reqwest::Client as HttpClient;
+use serenity::all::GuildId;
+use serenity::Client;
+use serenity::client::FullEvent;
+use serenity::prelude::*;
+use songbird::SerenityInit;
+
+mod music_commands;
+pub mod yt_search;
+
 // Types used by all command functions
 type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
+type CommandContext<'a> = poise::Context<'a, Data, Error>;
 
 struct HttpKey;
 
@@ -20,13 +23,10 @@ impl TypeMapKey for HttpKey {
     type Value = HttpClient;
 }
 
-pub struct GuildData {
-    current_track: TrackHandle,
-}
-
 // Custom user data passed to all command functions
 pub struct Data {
-    per_guild_data: Mutex<HashMap<GuildId, GuildData>>,
+    yt_api_key: Option<String>,
+    per_guild_data: Mutex<HashMap<GuildId, ()>>,
 }
 
 async fn on_poise_error(error: poise::FrameworkError<'_, Data, Error>) {
@@ -35,17 +35,82 @@ async fn on_poise_error(error: poise::FrameworkError<'_, Data, Error>) {
     }
 }
 
+async fn on_api_event(
+    ctx: &Context,
+    event: &FullEvent,
+    framework: FrameworkContext<'_, Data, Error>,
+    _data: &Data,
+) -> Result<(), Error> {
+    match event {
+        FullEvent::CacheReady { guilds } => {
+            // Print startup info
+            info!("Logged in as {}", ctx.cache.current_user().name);
+            if guilds.len() < 10 {
+                info!(
+                    "Active on these guilds: {}",
+                    guilds
+                        .iter()
+                        .map(|g| format!("{}<{}>", ctx.cache.guild(g).unwrap().name, g))
+                        .collect::<Vec<String>>()
+                        .join(", ")
+                );
+            } else {
+                info!("Active on {} guilds", guilds.len())
+            }
+        }
+        // Leave empty voice channels automatically
+        FullEvent::VoiceStateUpdate { new, .. } => {
+            let songbird = songbird::get(ctx).await.unwrap();
+
+            let guild_id = match new.guild_id {
+                Some(guild_id) => guild_id,
+                None => return Ok(()),
+            };
+
+            let call_lock = match songbird.get(guild_id) {
+                Some(call_clock) => call_clock,
+                None => return Ok(()),
+            };
+            let mut call = call_lock.lock().await;
+
+            // Check if the bot is the only one left in its channel
+            let should_leave = call.current_channel().is_some_and(|channel_id| {
+                let guild = ctx.cache.guild(guild_id).unwrap();
+
+                !guild.voice_states.iter().any(|(_, state)| {
+                    state.channel_id == Some(channel_id.0.into())
+                        && state.user_id != framework.bot_id
+                })
+            });
+
+            if should_leave {
+                call.queue().stop();
+                call.stop();
+                call.leave()
+                    .await
+                    .expect("Failed to leave channel after last user");
+            }
+        }
+        _ => {}
+    };
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    if env::var("RUST_LOG").is_ok() {
+        env_logger::init();
+    } else {
+        env_logger::Builder::new()
+            .filter(Some("gerbot"), LevelFilter::Info)
+            .init();
+    }
 
     let token = env::var("DISCORD_TOKEN").expect("Missing `DISCORD_TOKEN` env var");
 
-    // FrameworkOptions contains all of poise's configuration option in one struct
-    // Every option can be omitted to use its default value
+    // Create framework configuration
     let options = poise::FrameworkOptions {
         commands: vec![music_commands::help(), music_commands::play()],
-        // The global error handler for all error cases that may occur
         on_error: |error| Box::pin(on_poise_error(error)),
         // This code is run before every command
         pre_command: |ctx| {
@@ -58,15 +123,19 @@ async fn main() {
                 );
             })
         },
+        event_handler: |ctx, event, framework, data| {
+            Box::pin(on_api_event(ctx, event, framework, data))
+        },
         ..Default::default()
     };
 
+    // Build framework
     let framework = poise::Framework::builder()
         .setup(move |ctx, _ready, framework| {
             Box::pin(async move {
-                println!("Logged in as {}", _ready.user.name);
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
                 Ok(Data {
+                    yt_api_key: std::env::var("YOUTUBE_API_KEY").ok(),
                     per_guild_data: Mutex::new(HashMap::new()),
                 })
             })
@@ -74,7 +143,7 @@ async fn main() {
         .options(options)
         .build();
 
-    // Create a new instance of the Client, logging in as a bot.
+    // Create client config
     let mut client = Client::builder(&token, GatewayIntents::empty())
         .intents(GatewayIntents::non_privileged())
         .framework(framework)
@@ -83,8 +152,6 @@ async fn main() {
         .await
         .expect("Err creating client");
 
-    // Start listening for events by starting a single shard
-    if let Err(why) = client.start().await {
-        println!("Client error: {why:?}");
-    }
+    // Start client
+    client.start().await.unwrap();
 }
--- src/music_commands.rs
+++ src/music_commands.rs
@@ -1,12 +1,24 @@
-use crate::{Context, Error, GuildData};
+use std::ops::Deref;
+use std::sync::Arc;
+
+use log::error;
 use poise::{CreateReply, ReplyHandle};
 use reqwest::Client as HttpClient;
-use serenity::builder::CreateEmbed;
+use serenity::all::{ChannelId, GuildId};
+use serenity::builder::{AutocompleteChoice, CreateEmbed};
 use serenity::model::Colour;
+use songbird::{Call, Songbird};
+use songbird::error::JoinError;
 use songbird::input::YoutubeDl;
+use thiserror::Error;
+use tokio::sync::Mutex;
+
+use crate::{CommandContext, Error};
 
 const RESPONSE_COLOUR: Colour = Colour::BLURPLE;
 const ERROR_COLOUR: Colour = Colour::RED;
+
+// ======== Util functions ========
 
 async fn get_http_client(ctx: &serenity::client::Context) -> HttpClient {
     let data = ctx.data.read().await;
@@ -15,23 +27,70 @@ async fn get_http_client(ctx: &serenity::client::Context) -> HttpClient {
         .expect("Guaranteed to exist in the typemap.")
 }
 
+fn get_author_voice_state(ctx: CommandContext<'_>) -> (GuildId, Option<ChannelId>) {
+    let guild = ctx.guild().unwrap();
+    let channel_id = guild
+        .voice_states
+        .get(&ctx.author().id)
+        .and_then(|voice_state| voice_state.channel_id);
+
+    (guild.id, channel_id)
+}
+
 fn map_err<T>(send_result: Result<T, serenity::Error>) -> Result<T, Error> {
     send_result.map_err(|e| Box::new(e) as Error)
 }
 
 async fn respond_err<'a>(
-    ctx: &'a Context<'a>,
+    ctx: &'a CommandContext<'a>,
     details: impl Into<String>,
 ) -> Result<ReplyHandle<'a>, Error> {
     let embed = CreateEmbed::new()
         .title("Fehler")
         .colour(ERROR_COLOUR)
         .field("Details", details, false);
+
     map_err(
         ctx.send(CreateReply::default().embed(embed).ephemeral(true))
             .await,
     )
 }
+
+// ======== Shared components ========
+
+#[derive(Error, Debug)]
+enum JoinVoiceError {
+    #[error("Failed to join")]
+    Join(#[from] JoinError),
+    #[error("Did not join because the bot is used in another channel")]
+    Occupied,
+}
+
+/// Makes the bot join a specific voice channel, if it is not already in a different one
+async fn join_voice(
+    songbird: impl Deref<Target = Songbird>,
+    guild_id: GuildId,
+    channel_id: ChannelId,
+) -> Result<Arc<Mutex<Call>>, JoinVoiceError> {
+    if let Some(call) = songbird.get(guild_id) {
+        let current_channel = call.lock().await.current_channel();
+
+        // Already in the channel
+        if current_channel.is_some_and(|c| c == channel_id.into()) {
+            return Ok(call);
+        }
+
+        // Used in a different channel
+        if current_channel.is_some_and(|c| c != channel_id.into()) {
+            return Err(JoinVoiceError::Occupied);
+        }
+    }
+
+    // Bot not in a channel -> join
+    Ok(songbird.join(guild_id, channel_id).await?)
+}
+
+// ======== Commands ========
 
 /// Infos about the available commands
 #[poise::command(
@@ -40,7 +99,7 @@ async fn respond_err<'a>(
     ephemeral,
     description_localized("de", "Infos zu den verfügbaren Commands")
 )]
-pub async fn help(ctx: Context<'_>) -> Result<(), Error> {
+pub async fn help(ctx: CommandContext<'_>) -> Result<(), Error> {
     let listed_commands = ctx
         .framework()
         .options
@@ -69,41 +128,61 @@ pub async fn help(ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
 
+async fn autocomplete_yt_search(ctx: CommandContext<'_>, partial: &str) -> Vec<AutocompleteChoice> {
+    if partial.len() < 3 || partial.starts_with("http") {
+        // Discord doesn't like 0-length options
+        return vec![AutocompleteChoice::new("Tippe weiter, um Suchvorschläge zu erhalten", partial)];
+    }
+
+    let http_client = get_http_client(ctx.serenity_context()).await;
+    let yt_api_key = &ctx.data().yt_api_key;
+
+    match crate::yt_search::yt_search(partial, 5, http_client, yt_api_key.as_deref()).await {
+        Ok(results) => results
+            .into_iter()
+            .map(|video| AutocompleteChoice::new(&video.title, video.get_yt_url().as_str()))
+            .collect(),
+        Err(e) => {
+            error!("YT search failed: {:?}", e);
+            vec![AutocompleteChoice::new(partial, partial)]
+        }
+    }
+}
+
 //TODO: Help command text
-//Spielt ein Lied im momentanen Channel ab. Als Quelle geht ein Suchbegriff für Youtube oder ein direkter Link zu allen von yt-dlp unterstützten [Platformen](https://github.com/yt-dlp/yt-dlp/blob/master/supportedsites.md). Standartmäßig wird das Lied hinten in die Warteschlange eingereiht. Mit skip_queue true (TAB drücken nach Commandeingabe) wird es vorne eingereiht und sofort abgespielt (überspringt das momentane Lied)
+//Spielt ein Lied im momentanen Sprachkanal ab. Als Quelle geht ein Suchbegriff für Youtube oder ein direkter Link zu allen von yt-dlp unterstützten [Platformen](https://github.com/yt-dlp/yt-dlp/blob/master/supportedsites.md). Standartmäßig wird das Lied hinten in die Warteschlange eingereiht. Mit skip_queue true (TAB drücken nach Commandeingabe) wird es vorne eingereiht und sofort abgespielt (überspringt das momentane Lied)
 
 /// Plays a song in your current voice channel
 #[poise::command(
     slash_command,
     guild_only,
-    description_localized("de", "Spielt ein Lied im momentanen Voice Channel ab")
+    description_localized("de", "Spielt ein Lied im momentanen Sprachkanal ab")
 )]
 pub async fn play(
-    ctx: Context<'_>,
+    ctx: CommandContext<'_>,
     #[description = "Youtube search or direct link to all platforms supported by yt-dlp"]
     #[description_localized(
         "de",
         "Youtube-Suche oder Direktlink zu allen von yt-dlp unterstützten Platformen"
     )]
+    #[autocomplete = "autocomplete_yt_search"]
     source: String,
     #[description = "If the queue should be skipped"]
     #[description_localized("de", "Ob die Warteschlange übersprungen werden soll")]
     skip_queue: Option<bool>,
 ) -> Result<(), Error> {
+    //TODO: Implement SkipQueue
+    if skip_queue.is_some_and(|v| v) {
+        _ = respond_err(&ctx, "SkipQueue ist momentan nicht verfügbar").await?;
+        return Ok(());
+    }
+
     // ======== Join the right voice channel or return ========
 
     // Get user's current voice channel
-    let (guild_id, channel_id) = {
-        let guild = ctx.guild().unwrap();
-        let channel_id = guild
-            .voice_states
-            .get(&ctx.author().id)
-            .and_then(|voice_state| voice_state.channel_id);
+    let (guild_id, channel_id) = get_author_voice_state(ctx);
 
-        (guild.id, channel_id)
-    };
-
-    // Return in user not in a voice channel
+    // Return if user not in a voice channel
     let connect_to = match channel_id {
         Some(channel) => channel,
         None => {
@@ -115,26 +194,19 @@ pub async fn play(
     let songbird = songbird::get(ctx.serenity_context()).await.unwrap();
 
     // Make sure the bot is in the right channel
-    let call = if let Some(call) = songbird.get(guild_id) {
-        let current_channel = call.lock().await.current_channel();
-        if current_channel.is_some_and(|c| c != connect_to.into()) {
+    let call = match join_voice(songbird, guild_id, connect_to).await {
+        Ok(c) => c,
+        Err(JoinVoiceError::Join(e)) => {
+            _ = respond_err(&ctx, "Der Bot konnte deinem Sprachkanal nicht beitreten").await?;
+            return Err(e.into());
+        }
+        Err(JoinVoiceError::Occupied) => {
             _ = respond_err(
                 &ctx,
                 "Der Bot wird bereits in einem anderen Sprachkanal verwendet",
             )
             .await?;
             return Ok(());
-        }
-        call
-    } else {
-        // Bot not in a channel -> join
-        match songbird.join(guild_id, connect_to).await {
-            Ok(c) => c,
-            Err(_) => {
-                _ = respond_err(&ctx, "Der Bot konnte deinem Sprachkanal nicht beitreten").await?;
-                //TODO: Maybe don't return ok on join fail
-                return Ok(());
-            }
         }
     };
 
@@ -145,26 +217,18 @@ pub async fn play(
 
     // yt-dlp lookup
     let src = if do_search {
+        // This only available as a fallback for when autocomplete fails completely
         YoutubeDl::new_search(http_client, source)
     } else {
         YoutubeDl::new(http_client, source)
     };
 
-    // Play and save track
-    {
-        let track_handle = call.lock().await.play_input(src.clone().into());
-        let mut guild_data_map = ctx.data().per_guild_data.lock().await;
+    map_err(ctx.defer().await)?;
 
-        if let Some(guild_data) = guild_data_map.get_mut(&guild_id) {
-            guild_data.current_track = track_handle;
-        } else {
-            guild_data_map.insert(
-                guild_id,
-                GuildData {
-                    current_track: track_handle,
-                },
-            );
-        }
+    // Play track
+    {
+        let mut call = call.lock().await;
+        let _track_handle = call.enqueue_input(src.into()).await;
     }
 
     let embed = CreateEmbed::new()
